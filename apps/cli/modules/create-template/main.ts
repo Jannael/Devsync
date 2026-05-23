cec0e48ecd5316595551f3bd7fb0672de85df21f
Jannael Orlando <orlandojannael@gmail.com>
2026-05-23 16:29:05 -0600

    chore: update zod install warning

--- apps/cli/modules/create-template/main.ts
+++ apps/cli/modules/create-template/main.ts
@@ -4,6 +4,7 @@ import { CHECK, SPACE } from '@/utils/icons-terminal'
 import PrintASCII from '@/ascii'
 import { existsSync } from 'fs'
 import { BOLD, YELLOW } from '@/utils/colors'
+import corePackageJson from './../../../@core/package.json'
 
 export default async function createTemplate() {
   PrintASCII()
@@ -11,7 +12,7 @@ export default async function createTemplate() {
   console.log(`${SPACE}Creating template...`)
 
   if (existsSync('./package.json')) {
-    message = `\n${SPACE}- ${YELLOW('you need to install zod')} run ${BOLD('npm i zod@3.25.76 -E')}`
+    message = `\n${SPACE}- ${YELLOW('you need to install zod')} run ${BOLD(`npm i zod@${corePackageJson.dependencies.zod} -E`)}`
   }
 
   await copyDirectory(DEVSYNC_DIRECTORY, process.cwd())
