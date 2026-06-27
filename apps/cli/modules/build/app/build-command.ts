f23230d8b2d186509517ab107d24916ee6384322
Jannael <orlandojannael@gmail.com>
2026-06-27 15:06:58 -0600

    refactor: use devsync config in build command

--- apps/cli/modules/build/app/build-command.ts
+++ apps/cli/modules/build/app/build-command.ts
@@ -6,8 +6,11 @@ import { validateDevsyncMixin } from '@/shared/infra/validate-devsync'
 import { createCVMixin } from '@/shared/app/build-cv'
 import { CreateAcademicsMixin } from '@/shared/app/create-academics'
 import { CreateLinkedinMixin } from '@/shared/app/create-linkedin'
+import { getDevsyncConfigMixin } from '@/shared/infra/get-devsync-config'
 import { availableLangs } from '@devsync/core'
 import { runBunCommand } from '@/utils/run-bun-command'
+import { GITHUB_STYLES } from '@/constants/github-profile-styles'
+import { DEVSYNC_CONFIG } from '@/constants/devsync-config-fields'
 /*
 get defaultLang and languages from cwd
 */
@@ -15,7 +18,7 @@ get defaultLang and languages from cwd
 class BaseUpdateCommand {}
 
 class UpdateCommand extends CreateLinkedinMixin(
-	CreateAcademicsMixin(createCVMixin(CreateGithubProfileMixin(validateDevsyncMixin(BaseUpdateCommand)))),
+	CreateAcademicsMixin(createCVMixin(CreateGithubProfileMixin(getDevsyncConfigMixin(validateDevsyncMixin(BaseUpdateCommand))))),
 ) {
 	constructor() {
 		super()
@@ -35,12 +38,21 @@ class UpdateCommand extends CreateLinkedinMixin(
 			const languages = Object.keys(devsync).filter((key) => availableLangs.includes(key as (typeof availableLangs)[number]))
 			const defaultLang = devsync.defaultLang ?? 'en'
 
+			const cvPath = await this.getDevsyncConfig({ field: DEVSYNC_CONFIG.pathToCompiledCV })
+			const githubProfileStyle = await this.getDevsyncConfig({
+				field: DEVSYNC_CONFIG.githubProfileStyle,
+				required: false,
+				defaultValue: GITHUB_STYLES.default,
+			}) // minimal with fallback to default if not set
+
+			await this.createAcademics({ devsync, defaultLang })
+
 			for (const lang of languages) {
-				await this.buildCV({ name: devsync.name, lang })
+				await this.buildCV({ name: devsync.name, lang, path: cvPath })
 				await this.createLinkedin({ devsync, lang })
 			}
-			await this.createGithubProfile({ devsync, defaultLang })
-			await this.createAcademics({ devsync, defaultLang })
+
+			if (githubProfileStyle === GITHUB_STYLES.default) await this.createGithubProfile({ devsync, defaultLang })
 
 			console.log(`${SPACE}${CHECK(`${BOLD('Build process completed successfully.')}`)}`)
 		} catch (e) {
--- apps/cli/shared/app/build-cv.ts
+++ apps/cli/shared/app/build-cv.ts
@@ -4,14 +4,13 @@ import { CHECK, SPACE } from '@/utils/icons-terminal'
 import { CV_PDF } from '@/constants/paths'
 import { createPDFMixin } from '@/shared/infra/create-pdf'
 import { getHTMLMixin } from '@/shared/infra/get-html'
-import { $ } from 'bun'
 
-// to get the CV route we get it from package.json (devsync.pathToCompiledCv)
+// the CV path is obtained from package.json (devsync.pathToCompiledCV)
 // and we replace [lang] with the current language
 export function createCVMixin<TBase extends GConstructor>(Base: TBase) {
 	return class extends getHTMLMixin(createPDFMixin(Base)) {
-		async buildCV({ name = 'CV', lang }: { name: string | undefined; lang: string }) {
-			const CVPath = (await $`bun pm pkg get devsync.pathToCompiledCV`.text()).trim().replace('[lang]', lang).replace(/"/g, '')
+		async buildCV({ name = 'CV', lang, path }: { name: string | undefined; lang: string; path: string }) {
+			const CVPath = path.replace('[lang]', lang)
 
 			console.log(`${SPACE}${GREEN('-')} Building CV and generating PDF...`)
 			const html = await this.getHTML({ path: CVPath })
